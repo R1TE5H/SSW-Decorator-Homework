c35f9f2ad02c01f8180b4d0687ef0a45577918c3
Ritesh Persaud <riteshpersaud2003@gmail.com>
2024-11-17 14:55:28 -0500

    First commit

---
+++ MargheritaPizza.java
@@ -0,0 +1,12 @@
+
+public class MargheritaPizza implements Pizza {
+
+    public String getDescription(){
+        return "Margherita Pizza";
+    }
+
+    public double getCost(){
+        return 6.99;
+    }
+
+}
